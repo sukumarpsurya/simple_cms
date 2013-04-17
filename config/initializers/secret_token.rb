e8fcf7d34ddf28df956a3c3fad7b1f0793022729
Sukumar Ponram <suku8u@uw.edu>
2013-04-17 00:38:27 -0700

    adding new files for the first time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = 'd3b578d2580b20b380a0db7d3003327f455bea0db68334b7fb3e5d48b89d00b88c91957b926ddf7c0b0b2f67b1648c94ec7f22d3fe61ca119a8ce559630351b8'
